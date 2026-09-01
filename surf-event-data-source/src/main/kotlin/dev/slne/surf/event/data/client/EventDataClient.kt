6765467c2eaf1122118c2422b435e5ebd03a70fe
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-09-01 21:52:06 +0200

    feat: implement placeholder for fetching active events

--- surf-event-data-source/src/main/kotlin/dev/slne/surf/event/data/client/EventDataClient.kt
+++ surf-event-data-source/src/main/kotlin/dev/slne/surf/event/data/client/EventDataClient.kt
@@ -1,8 +1,9 @@
 package dev.slne.surf.event.data.client
 
+import dev.slne.surf.event.data.EventData
+
 internal object EventDataClient {
 
-    suspend fun getActiveEvents(): List<EventData> {
-
-    }
+    suspend fun getActiveEvents(): List<EventData> =
+        TODO("Implement the logic to fetch active events from the data source")
 }
